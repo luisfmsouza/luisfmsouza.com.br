f61ce36a4791af4185929aead3ebc1d12194bee1
Luis Felipe <luisfmsouza@gmail.com>
2021-04-18 11:19:08 +0200

    Format cypress files and fix e2e test

--- cypress/integration/home_page.spec.js
+++ cypress/integration/home_page.spec.js
@@ -1,11 +1,9 @@
-describe("Visit my personal homepage", function() {
-  it("finds the name and description", function() {
+describe("Visit my personal homepage", function () {
+  it("finds the name and description", function () {
     cy.visit("/");
 
     cy.contains("Luís Felipe Souza");
-    cy.contains(
-      "Front-end Developer, traveler, beer lover and entrepreneur in my free time."
-    );
+    cy.contains("Front-end Engineer @ FindHotel. Based in Amsterdam 🇳🇱");
 
     cy.percySnapshot();
   });
