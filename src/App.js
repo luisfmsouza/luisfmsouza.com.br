75308e3d1ed48c6f607d2607e98195629e81585a
Luis Felipe <luisfmsouza@gmail.com>
2022-03-16 09:48:51 +0100

    Remove mentoring phrase

--- src/App.js
+++ src/App.js
@@ -2,64 +2,47 @@ import React from 'react';
 import './App.css';
 import { SocialIcon } from 'react-social-icons';
 
-const App = () => {
-  return (
-    <div className="container">
-      <header>
-        <h1 className="title">Luís Felipe Souza</h1>
-        <h2 className="headline">
-          Engineering Manager @{' '}
-          <a
-            href="https://careers.findhotel.net/?utm_source=luisfmsouza.com.br"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            FindHotel
-          </a>
-          . <br />
-          Based in Amsterdam 🇳🇱
-        </h2>
-      </header>
+const App = () => (
+  <div className="container">
+    <header>
+      <h1 className="title">Luís Felipe Souza</h1>
+      <h2 className="headline">
+        Engineering Manager @{' '}
+        <a
+          href="https://careers.findhotel.net/?utm_source=luisfmsouza.com.br"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          FindHotel
+        </a>
+        . <br />
+        Based in Amsterdam 🇳🇱
+      </h2>
+    </header>
 
-      <section className="content">
-        <ul className="social-list">
-          <li>
-            <SocialIcon
-              url="https://instagram.com/luisfmsouza"
-              target="_blank"
-            />
-          </li>
-          <li>
-            <SocialIcon url="https://twitter.com/luisfmsouz" target="_blank" />
-          </li>
-          <li>
-            <SocialIcon
-              url="https://linkedin.com/in/luisfmsouza"
-              target="_blank"
-            />
-          </li>
-          <li>
-            <SocialIcon url="https://github.com/luisfmsouza" target="_blank" />
-          </li>
-          <li>
-            <SocialIcon url="https://medium.com/@luisfmsouza" target="_blank" />
-          </li>
-        </ul>
-      </section>
-      <section className="content">
-        <h2 className="headline">
-          <a
-            href="https://mentora.webflow.io/mentor/luis-souza"
+    <section className="content">
+      <ul className="social-list">
+        <li>
+          <SocialIcon url="https://instagram.com/luisfmsouza" target="_blank" />
+        </li>
+        <li>
+          <SocialIcon url="https://twitter.com/luisfmsouz" target="_blank" />
+        </li>
+        <li>
+          <SocialIcon
+            url="https://linkedin.com/in/luisfmsouza"
             target="_blank"
-            rel="noopener noreferrer"
-          >
-            Book
-          </a>{' '}
-          30 minutes of mentoring (for free 🤑).
-        </h2>
-      </section>
-    </div>
-  );
-};
+          />
+        </li>
+        <li>
+          <SocialIcon url="https://github.com/luisfmsouza" target="_blank" />
+        </li>
+        <li>
+          <SocialIcon url="https://medium.com/@luisfmsouza" target="_blank" />
+        </li>
+      </ul>
+    </section>
+  </div>
+);
 
 export default App;
